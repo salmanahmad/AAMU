a5fccc502965c2dfdaaf2a72cb10f1817e9f1dda
Salman Ahmad <salman@salmanahmad.com>
2012-03-14 11:02:46 -0400

    Adding the backend stuff

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 source :rubygems
+
 gem 'sinatra'
 gem 'thin'
+gem 'data_mapper'
--- app.rb
+++ app.rb
@@ -1,7 +1,33 @@
-require 'rubygems' 
-require 'sinatra' 
+require 'rubygems'
+require 'sinatra'
+require 'data_mapper'
+require 'date'
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/db/results.db")
+
+class Trial
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :data, Text
+  property :created_at, DateTime
+end
+
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
 
 get '/' do 
   redirect '/index.html'
 end
 
+post '/trials.json' do
+  trial = Trial.new
+  trial.data = params[:data]
+  trial.created_at = DateTime.now
+  if trial.save then
+    200  
+  else
+    500
+  end
+end
